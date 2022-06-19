ed0deb57d7120f8ec40cf3af665a5d51ddcc08be
Anouk Dutree <anouk.dutree@dutchanalytics.com>
2022-06-19 15:52:03 +0200

    Added jumping and slow down functionality to rolling mechanics

--- Assets/Prototype/Anouk/Scripts/AD_PlayerController.cs
+++ Assets/Prototype/Anouk/Scripts/AD_PlayerController.cs
@@ -5,10 +5,15 @@ using UnityEngine.InputSystem;
 
 public class AD_PlayerController : MonoBehaviour
 {
-    public float speed = 0.0f;
+    public float speed = 20.0f;
+    public float sphereRadius = 1.0f;
+    public float jumpSpeed = 18.0f;
+    private float groundCheckRadius = 0.1f;
+    public LayerMask groundLayer;
     private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+    private Vector3 movement;
+    [SerializeField]
+    private float sensitivity = 0.2f;
     
     // Start is called before the first frame update
     void Start()
@@ -19,13 +24,41 @@ public class AD_PlayerController : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        movement = new Vector3(movementVector.x, 0.0f, movementVector.y);
     }
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        if (OnGround())
+        {
+            rb.AddForce(movement * speed);
+
+            // No input from player
+            if (movement.magnitude <= sensitivity && rb.velocity.magnitude > 0.0f)
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, speed * 0.1f * Time.deltaTime); // Slow down
+            }
+        }
     }
+
+    public void OnJump()
+    {
+        if (OnGround())
+        {
+            // allow jump only if player is currently grounded
+            rb.AddForce(Vector3.up * jumpSpeed , ForceMode.Impulse);
+        }
+    }
+
+    public bool OnGround()
+    {
+        return Physics.CheckSphere(transform.position - (Vector3.up * sphereRadius), groundCheckRadius, groundLayer);
+    }
+
+    // void OnDrawGizmosSelected()
+    // {
+    //     // For debugging
+    //     Gizmos.color = Color.yellow;
+    //     Gizmos.DrawSphere(transform.position - (Vector3.up * sphereRadius), groundCheckRadius);
+    // }
 }
