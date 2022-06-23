bbee5dc6d0c39c5dcd715a060b6462d53ec23c76
Anouk Dutree <anouk.dutree@dutchanalytics.com>
2022-06-23 21:59:22 +0200

    Added walking around logic for example animal

--- Assets/Prototype/Anouk/Scripts/AD_GameManager.cs
+++ Assets/Prototype/Anouk/Scripts/AD_GameManager.cs
@@ -24,7 +24,6 @@ public class AD_GameManager : MonoBehaviour
     {
         fruitScores[type]++;
         fruitScoresUI.UpdateScoresUI(type, fruitScores[type]);
-        Debug.Log(fruitScores[type]);
     }
 
     private void AddAnimalToCollection(string animalType)
--- Assets/Prototype/Anouk/Scripts/Item interaction/Animal.cs
+++ Assets/Prototype/Anouk/Scripts/Item interaction/Animal.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Animal : Interactable
 {
     public string animalType = "";
+    UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        StartCoroutine(WalkAround());
+    }
 
     // TODO: add walking around behavior
     
@@ -14,5 +22,19 @@ public class Animal : Interactable
         AD_EventManager.CollectAnimal(animalType);
         // Disable/Destroy
         Destroy(gameObject);    
+    }
+
+    private IEnumerator WalkAround()
+    {
+        while(gameObject.activeSelf)
+        {
+            Vector3 randomPoint = Random.insideUnitCircle * 10;
+            randomPoint += transform.position;
+            UnityEngine.AI.NavMeshHit hit;
+            UnityEngine.AI.NavMesh.SamplePosition(randomPoint, out hit, 10, 1);
+            Vector3 finalPosition = hit.position;
+            agent.SetDestination(finalPosition);
+            yield return new WaitForSeconds(4);
+        }
     }
 }
