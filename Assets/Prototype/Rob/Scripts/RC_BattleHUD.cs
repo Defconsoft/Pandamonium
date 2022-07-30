a906b13ef28f7757be9764635d393d205d495510
Rob Charlish <rob.charlish@gmail.com>
2022-07-30 13:58:08 +0100

    battle HUD in normal city

--- Assets/Prototype/Rob/Scripts/RC_BattleHUD.cs
+++ Assets/Prototype/Rob/Scripts/RC_BattleHUD.cs
@@ -5,19 +5,15 @@ using UnityEngine.UI;
 
 public class RC_BattleHUD : MonoBehaviour
 {
-    public TMPro.TMP_Text nameText, levelText;
-    public Slider hpSlider;
+    public Image hpSlider;
 
 
     public void SetHUD(RC_BattleUnit unit) {
-        nameText.text = unit.unitName;
-        levelText.text = "Lvl " + unit.unitLevel;
-        hpSlider.maxValue = unit.maxHP;
-        hpSlider.value = unit.currentHP;
+        hpSlider.fillAmount = unit.currentHP;
     }
 
-    public void SetHP (int hp){
-        hpSlider.value = hp;
+    public void SetHP (float hp){
+        hpSlider.fillAmount = hp;
     }
 
 
